a5c81276bd56387b65d1918d01c75f80fda0395e
verticalcoot2 <verticalcoot2@gmail.com>
2025-01-15 21:04:52 +0100

    SecondCommit

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ async function Lekerdzes(url)
 
 async function Build(target)
 {
-    let data = await Lekerdzes("https://api.imgflip.com/get_memes")
+    let data = await Lekerdzes("https://api.imgflip.com/get_memes");
     let adat = data.data.memes;
     for(let i = 0; i < adat.length; i++)
     {
@@ -31,8 +31,11 @@ async function Build(target)
             let kep = document.createElement("img");
             kep.classList.add("kep")
             kep.src = adat[i].url;
+            //avgPXL = get_average_rgb(kep.src);
+            //console.log(avgPXL);
+            kepD.dataset.r = 
+
             kep.alt = adat[i].name;
-            kep.onmouseover = adat[i].name;    
 
             kepD.appendChild(kep)
 
@@ -49,10 +52,33 @@ async function Build(target)
 
         kepD.addEventListener("mouseover", function()
         {
-            target.style.boxShadow = getAverageRGB(kep);
-            console.log("kaki");
+            var k = get_average_rgb(adat[this.dataset.index].url);
+            let oink = "0px 0px 30px 20px rgba("+k[0]+", "+k[1]+", "+k[2]+", 1)";
+            kepD.style.boxShadow = oink
+            console.log(k[0]+ "," + k[1]+ "," + k[2]);
         })
+
+        kepD.addEventListener("mouseleave", function()
+        {
+            var k = get_average_rgb(adat[this.dataset.index].url);
+            let oink = "0px 0px 0px 0px rgba("+k[0]+", "+k[1]+", "+k[2]+", 1)";
+            kepD.style.boxShadow = oink;
+            console.log(k[0]+ "," + k[1]+ "," + k[2]);
+        });
     }
+}
+
+function get_average_rgb(img) {
+    var context = document.createElement('canvas').getContext('2d');
+    if (typeof img == 'string') {
+        var src = img;
+        img = new Image;
+        img.setAttribute('crossOrigin', ''); 
+        img.src = src;
+    }
+    context.imageSmoothingEnabled = true;
+    context.drawImage(img, 0, 0, 1, 1);
+    return context.getImageData(0, 0, 1, 1).data.slice(0,3);
 }
 
 function mData(data)
@@ -71,50 +97,4 @@ function mData(data)
     _kep.style.width = data.width;
     _kep.style.height = data.height;
     _kepD.appendChild(_kep);
-}
-
-function getAverageRGB(imgEl) {
-
-    var blockSize = 5, // only visit every 5 pixels
-        defaultRGB = {r:0,g:0,b:0}, // for non-supporting envs
-        canvas = document.createElement('canvas'),
-        context = canvas.getContext && canvas.getContext('2d'),
-        data, width, height,
-        i = -4,
-        length,
-        rgb = {r:0,g:0,b:0},
-        count = 0;
-
-    if (!context) {
-        return defaultRGB;
-    }
-
-    height = canvas.height = imgEl.naturalHeight || imgEl.offsetHeight || imgEl.height;
-    width = canvas.width = imgEl.naturalWidth || imgEl.offsetWidth || imgEl.width;
-
-    context.drawImage(imgEl, 0, 0);
-
-    try {
-        data = context.getImageData(0, 0, width, height);
-    } catch(e) {
-        /* security error, img on diff domain */
-        return defaultRGB;
-    }
-
-    length = data.data.length;
-
-    while ( (i += blockSize * 4) < length ) {
-        ++count;
-        rgb.r += data.data[i];
-        rgb.g += data.data[i+1];
-        rgb.b += data.data[i+2];
-    }
-
-    // ~~ used to floor values
-    rgb.r = ~~(rgb.r/count);
-    rgb.g = ~~(rgb.g/count);
-    rgb.b = ~~(rgb.b/count);
-
-    return rgb;
-
 }
